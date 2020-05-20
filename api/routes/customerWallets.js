d604790ede0cc3018bd17b2f8ffaeee98e8f3ea4
Gabriel Macias <gabriel_pedernera@hotmail.com.ar>
2020-05-20 17:06:07 -0300

    post method add

--- api/routes/customerWallets.js
+++ api/routes/customerWallets.js
@@ -3,5 +3,6 @@ module.exports = app => {
     const controller = app.controllers.customerWallets;
 
     app.route('/api/v1/customer-wallets')
-    .get(controller.lisCustomerWallets);
+    .get(controller.lisCustomerWallets)
+    .post(controller.saveCustomerWallets);
 }
