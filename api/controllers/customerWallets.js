fbdc71260c228018d104c73dd30dbfc3f989d41b
Gabriel Macias <gabriel_pedernera@hotmail.com.ar>
2020-05-21 16:25:58 -0300

    add put method

--- api/controllers/customerWallets.js
+++ api/controllers/customerWallets.js
@@ -50,6 +50,44 @@ module.exports = app => {
         }
     };
 
+    controller.updateCustomerWallets = (req, res) => {
+        
+        const {
+            customerId,
+        } = req.params;
+
+        const foundCustomerIndex = customerWalletsMock.data.findIndex(customer => customer.id === customerId);
+
+        if(foundCustomerIndex === -1) {
+            res.status(404).json({
+                message: 'Customer not found',
+                success: false,
+                customerWallets: customerWalletsMock,
+            });
+        }else{
+            const newCustomer = {
+                id: customerId,
+                parentId: req.body.parentId,
+                name: req.body.name,
+                birthDate: req.body.birthDate,
+                cellphone: req.body.cellphone,
+                phone: req.body.phone,
+                email: req.body.email,
+                occupation: req.body.occupation,
+                status: req.body.status,
+                createdAt: new Date()
+            };
+
+            customerWalletsMock.data.splice(foundCustomerIndex, 1, newCustomer);
+
+            res.status(200).json({
+                message: 'Customer updated successfully',
+                success: true,
+                customer: customerWalletsMock,
+            });
+        }
+    }
+
     return controller;
 
 }
--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./config/express')();
 const port = app.get('port');
 
 app.listen(port, () => {
-    console.log(`>> server listening on port ${port}`)
+    console.log(`>> server listening on port ${port} <<`)
 });
